312a4cac2af1a0f7d3ee5ba55b928c97b5d9945f
MartinDiCe <jamartindice@gmail.com>
2025-04-25 10:51:51 -0300

    Parameters Swagger && HangFire and new toglemiddleware

---
+++ Middleware/FeatureToggleMiddleware.cs
@@ -0,0 +1,25 @@
+﻿using SPOrchestratorAPI.Models.Repositories.ParameterRepositories;
+
+namespace SPOrchestratorAPI.Middleware
+{
+    /// <summary>
+    /// Bloquea o deja pasar la request según el valor de un parámetro en BD.
+    /// </summary>
+    public class FeatureToggleMiddleware(RequestDelegate next, string parameterName)
+    {
+        public async Task InvokeAsync(HttpContext context)
+        {
+            var repo = context.RequestServices.GetRequiredService<IParameterRepository>();
+            var param = await repo.GetByNameAsync(parameterName);
+
+            if (param == null ||
+                !param.ParameterValue.Equals("true", StringComparison.OrdinalIgnoreCase))
+            {
+                context.Response.StatusCode = StatusCodes.Status404NotFound;
+                return;
+            }
+
+            await next(context);
+        }
+    }
+}
