c651d224c77322d7ff380b0083d3c17f9c2a2fd7
MartinDiCe <jamartindice@gmail.com>
2025-08-08 14:22:03 -0300

    new db refactor

--- Configuration/AllowAllDashboardAuthorizationFilter.cs
+++ Configuration/AllowAllDashboardAuthorizationFilter.cs
@@ -1,8 +1,20 @@
 ﻿using Hangfire.Dashboard;
 
-namespace SPOrchestratorAPI.Configuration;
-
-public class AllowAllDashboardAuthorizationFilter : IDashboardAuthorizationFilter
+namespace SPOrchestratorAPI.Configuration
 {
-    public bool Authorize(DashboardContext context) => true;
+    /// <summary>
+    /// Authorization filter for Hangfire Dashboard that allows all requests.
+    /// Implement this when you want to expose the dashboard without authentication.
+    /// </summary>
+    public class AllowAllDashboardAuthorizationFilter : IDashboardAuthorizationFilter
+    {
+        /// <summary>
+        /// Determines whether the current HTTP context is authorized to access the Hangfire Dashboard.
+        /// </summary>
+        /// <param name="context">Contextual information about the dashboard request.</param>
+        /// <returns>
+        /// <c>true</c> to allow any user to access the dashboard; otherwise, <c>false</c>.
+        /// </returns>
+        public bool Authorize(DashboardContext context) => true;
+    }
 }
